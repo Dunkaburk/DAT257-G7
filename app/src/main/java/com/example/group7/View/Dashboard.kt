c918ed767b825ba51387509b7891a16afdef1757
02list15 <02list15@gmail.com>
2023-04-12 15:36:35 +0200

    Uhh

---
+++ app/src/main/java/com/example/group7/View/Dashboard.kt
@@ -0,0 +1,39 @@
+package com.example.group7.View
+
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.Role.Companion.Button
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun Dashboard(){
+    MenuButton()
+}
+
+@Composable
+fun MenuButton(){
+    Button(
+        onClick = { /* ... */ },
+        // Uses ButtonDefaults.ContentPadding by default
+        contentPadding = PaddingValues(
+            start = 20.dp,
+            top = 12.dp,
+            end = 20.dp,
+            bottom = 12.dp
+        )
+    ) {
+        // Inner content including an icon and a text label
+        Icon(
+            Icons.Filled.Favorite,
+            contentDescription = "Favorite",
+            modifier = Modifier.size(ButtonDefaults.IconSize)
+        )
+        Spacer(Modifier.size(ButtonDefaults.IconSpacing))
+        Text("Like")
+    }
+}
