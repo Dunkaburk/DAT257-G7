c455c9a1f20ba7fb3492b7231acea1f2d4172907
Dunkaburk <jonte8000@hotmail.com>
2023-04-14 15:10:57 +0200

    Added Topbar and scrolling functionality to Dashboard page

--- app/src/main/java/com/example/group7/View/Dashboard.kt
+++ app/src/main/java/com/example/group7/View/Dashboard.kt
@@ -8,31 +8,94 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.SmallTopAppBar
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.sp
+//import androidx.navigation.NavController
+import com.example.group7.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Dashboard(){
-    MenuButton()
+    AmbundiTopAppBar()
+
 }
 
+
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MenuButton(){
-    Button(
-        onClick = { /* Open menu slider action */ },
-        // Uses ButtonDefaults.ContentPadding by default
-        contentPadding = PaddingValues(
-            start = 20.dp,
-            top = 12.dp,
-            end = 20.dp,
-            bottom = 12.dp
-        )
-    ) {
-        // Inner content including an icon and a text label
-        Icon(
-            Icons.Filled.Menu,
-            contentDescription = "Menu",
-            modifier = Modifier.size(ButtonDefaults.IconSize)
-        )
-        Spacer(Modifier.size(ButtonDefaults.IconSpacing))
-        Text("Menu")
-    }
+fun AmbundiTopAppBar(/*navController: NavController*/) {
+    //val logo: Painter = painterResource(R.drawable.ambundi_logo)
+
+    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
+    Scaffold(
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            SmallTopAppBar(
+                title = {
+                    Text(
+                        "Ambundi",
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis,
+                        letterSpacing = 2.sp
+                    )
+                },
+                navigationIcon = {
+                    IconButton(onClick = { /* doSomething() */ }) {
+                        Icon(
+                            imageVector = Icons.Filled.Delete, // TODO INSERT APP ICON HERE
+                            contentDescription = "Localized description"
+                        )
+                    }
+                },
+                actions = {
+                    IconButton(
+                        onClick = { /* TODO Ta upp meny*/ },
+                        modifier = Modifier
+                            .padding(end = 8.dp)
+                            .align(Alignment.CenterVertically)
+                            .weight(1f, fill = false)
+                    ) {
+                        Icon(Icons.Filled.Menu, contentDescription = "Menu")
+                    }
+                },
+                scrollBehavior = scrollBehavior
+            )
+        },
+        content = { innerPadding ->
+            LazyColumn(
+                contentPadding = innerPadding,
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                val list = (0..75).map { it.toString() } //Helt arbiträrt antal listItems för att visa hur man kan scrolla
+                items(count = list.size) {
+                    Text(
+                        text = list[it],
+                        style = MaterialTheme.typography.bodyLarge,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 16.dp)
+                    )
+                }
+            }
+        }
+    )
 }
+
+
+
+//------------------------------------------------------------
