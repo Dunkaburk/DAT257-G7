e92af91414ba43bdd3514fa0f00197c816a9ec1a
låke <91898187+lutches@users.noreply.github.com>
2023-04-19 15:05:53 +0200

    added a simple view for updating body info. needs needs further functionality and alot of styling.

--- app/src/main/java/com/example/group7/View/bodyview.kt
+++ app/src/main/java/com/example/group7/View/bodyview.kt
@@ -1,20 +1,50 @@
 package com.example.group7.View
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.*
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.example.group7.Model.BodyInfo
-import java.lang.reflect.Modifier
 
-
+@Preview
 @Composable
-fun bodyview(bodyInfo: BodyInfo) {
+fun BodyView() {
+    Column {
+        Row {
+            Text(text = "Height")
+            simpleTextField()
+        }
+        Row {
+            Text(text = "Weight")
+            simpleTextField()
+        }
+        Row {
+            Text(text = "Age")
+            simpleTextField()
+        }
 
-    Text(
-        text = "Body Info"
-    )
+        updatebodyinfobutton()
+    }
 }
 
+@Composable
+
+fun updatebodyinfobutton() {
+    Button(onClick = { /*TODO*/ }) {
+        Text(text = "Update Body Info")
+    }
+}
+@Composable
+fun simpleTextField() {
+    var previousValue by remember { mutableStateOf(TextFieldValue("")) }
+    TextField(
+        value = previousValue,
+        onValueChange = {
+            previousValue = it
+        },
+        placeholder = { Text(text = "Your Placeholder") },
+    )
+}
