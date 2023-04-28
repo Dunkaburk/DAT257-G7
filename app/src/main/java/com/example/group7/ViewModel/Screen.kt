152accb1b7e294d2c6ac70a178986f89e363f626
Stocken <123vincentstocks@gmail.com>
2023-04-28 11:17:28 +0200

    NavController implemented v2

--- app/src/main/java/com/example/group7/ViewModel/Screen.kt
+++ app/src/main/java/com/example/group7/ViewModel/Screen.kt
@@ -2,6 +2,5 @@ package com.example.group7.ViewModel
 
 sealed class Screen(val route: String) {
     object Dashboard: Screen(route = "dashboard_screen")
-    object MainActivity: Screen(route = "mainactivity_screen")
     object StartScreen: Screen(route = "startscreen_screen")
 }
