04bd8da48b684c3823073b8d7ee5e597c367c5f7
02list15 <02list15@gmail.com>
2023-05-07 21:47:08 +0200

    Improved StepsPanel.kt and added the EO-logo the top bar in Dashboard and StepsPanel. Also deleted StepsView since it was a duplicate of StepsPanel.

--- app/src/main/java/com/example/group7/View/StepsPanel.kt
+++ app/src/main/java/com/example/group7/View/StepsPanel.kt
@@ -1,7 +1,6 @@
  package com.example.group7.View
 
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.text.KeyboardActions
@@ -14,17 +13,20 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.group7.R
+
  @Preview
  @Composable
  fun StepsView(/*navController: NavController*/) {
@@ -34,14 +36,12 @@ import com.example.group7.R
  @OptIn(ExperimentalMaterial3Api::class)
  @Composable
  fun StepsContent(/*navController: NavController*/) {
-     //val logo: Painter = painterResource(R.drawable.ambundi_logo)
+     val logo: Painter = painterResource(R.drawable.eologo)
      var stepsGoal by remember { mutableStateOf(10000) }
      var stepCount by remember { mutableStateOf(1000) } // TODO get from pedometer
-     var sleepGoal by remember { mutableStateOf((8*60)) }
-     var sleepCount by remember { mutableStateOf((2*60)) }
      var progress by remember { mutableStateOf(0.2f) }
      var showDialog by remember { mutableStateOf(false) }
-     var hasIndividualGoal by remember { mutableStateOf(true) }
+     var hasIndividualGoal by remember { mutableStateOf(false) } // TODO make getter and setter for this variable and set to false first time a user opens app and when goal is completed
      var goalProgress by remember { mutableStateOf(((stepCount/stepsGoal).toFloat())) }
 
      val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
@@ -58,10 +58,10 @@ import com.example.group7.R
                      )
                  },
                  navigationIcon = {
-                     IconButton(onClick = { /* doSomething() */ }) {
-                         Icon(
-                             imageVector = Icons.Filled.Delete, // TODO INSERT APP ICON HERE
-                             contentDescription = "Localized description"
+                     IconButton(onClick = { /* Send me to Dashboard */ }) {
+                         Image(
+                             painter = painterResource(id = R.drawable.eologo),
+                             contentDescription = null // decorative element
                          )
                      }
                  },
@@ -84,7 +84,7 @@ import com.example.group7.R
              contentPadding = innerPadding,
              verticalArrangement = Arrangement.spacedBy(8.dp)
          ) {
-             item { GoalProgressCard() }
+             //item { GoalProgressCard() } @Tantan snälla kolla på det här vi pajar allt om vi tar bort det :(((
              if (hasIndividualGoal) item { IndividualGoalsCard( { showDialog = true }, goalProgress )}
              if (showDialog) item {
                  CustomGoalPopup(
@@ -100,14 +100,14 @@ import com.example.group7.R
      }
  }
 
-@OptIn(ExperimentalMaterial3Api::class)
+/*@OptIn(ExperimentalMaterial3Api::class)
 @Composable
- fun GoalProgressCard() {
+ fun GoalProgressCard() { // Unsure if we should scrap daily goals for now and replace it with chosen goal
      Card(
          modifier = Modifier
              .fillMaxWidth()
              .padding(top = 2.dp, bottom = 2.dp, start = 15.dp, end = 15.dp)
-             .clickable { /* TODO Navigate to chosen screen */ },
+             .clickable { *//* TODO Navigate to chosen screen *//* },
          elevation = CardDefaults.cardElevation(
              defaultElevation = 10.dp),
          border = BorderStroke(1.dp, Color(0xFF000000)),
@@ -141,7 +141,7 @@ import com.example.group7.R
              )
          }
      }
- }
+ }*/
 
  @OptIn(ExperimentalMaterial3Api::class)
  @Composable
@@ -168,7 +168,7 @@ import com.example.group7.R
                  )
                  {
                      Text(
-                         text = "Individual goals",
+                         text = "Goal",
                          textAlign = TextAlign.Center,
                          style = MaterialTheme.typography.headlineSmall,
                          modifier = Modifier.align(Alignment.Center)
@@ -259,6 +259,8 @@ import com.example.group7.R
  }
 
 
+
+
  @OptIn(ExperimentalMaterial3Api::class)
  @Composable
  fun ChooseGoalPanel(OnClickAction: () -> Unit){
@@ -267,13 +269,219 @@ import com.example.group7.R
          .padding(top = 2.dp, bottom = 2.dp, start = 15.dp, end = 15.dp),
          elevation = CardDefaults.cardElevation(
              defaultElevation = 10.dp)){
+         Text(
+             text = "Choose the type of goal",
+             textAlign = TextAlign.Center,
+             style = MaterialTheme.typography.headlineSmall,
+             modifier = Modifier.align(Alignment.CenterHorizontally)
+         )
+         var tabIndex by remember { mutableStateOf(0) }
+
+         val tabs = listOf("City to city", "Imaginative", "Custom")
+
+         Column(modifier = Modifier.fillMaxWidth()) {
+             TabRow(selectedTabIndex = tabIndex) {
+                 tabs.forEachIndexed { index, title ->
+                     Tab(text = { Text(title) },
+                         selected = tabIndex == index,
+                         onClick = { tabIndex = index }
+                     )
+                 }
+             }
+             when (tabIndex) {
+                 0 -> CityToCityTab()
+                 1 -> ImaginativeGoalsTab()
+                 2 -> CustomStepsTab()
+             }
+         }
          OutlinedButton(
-             onClick = {OnClickAction()},
+             onClick = {/* TODO save current goal */},
              modifier = Modifier
                  .padding(10.dp)
                  .align(Alignment.CenterHorizontally))
          {
-             Text("Choose goal", fontSize = 18.sp)
+             Text("Save goal", fontSize = 18.sp)
+             //hasIndividualGoal = true
+         }
+     }
+ }
+
+ @Composable
+ fun CustomStepsTab() {
+     var text by remember { mutableStateOf(TextFieldValue("")) }
+     Row(
+         modifier = Modifier.fillMaxWidth()){
+         TextField(
+             modifier = Modifier.padding(start= 32.dp),
+             value = text,
+             onValueChange = {
+                 text = it
+             },
+             label = { Text(text = "Number of steps") },
+             placeholder = { Text(text = "E.g. 10000") },
+         )
+     }
+
+
+ }
+
+ @Composable
+ fun ImaginativeGoalsTab() {
+     DropDownMenuImaginativeGoals()
+ }
+
+ @Composable
+ fun CityToCityTab() {
+     DropDownMenuCityStart()
+     DropDownMenuCityDestination()
+ }
+
+ @OptIn(ExperimentalMaterial3Api::class)
+ @Composable
+ fun DropDownMenuCityStart() {
+     val context = LocalContext.current
+     val cityDestinations = arrayOf("Stockholm", "Copenhagen", "Helsinki", "London", "Paris")
+     var expanded by remember { mutableStateOf(false) }
+     var selectedText by remember { mutableStateOf("") }
+
+     Box(
+         modifier = Modifier
+             .fillMaxWidth()
+             .padding(32.dp)
+     ) {
+         ExposedDropdownMenuBox(
+             expanded = expanded,
+             onExpandedChange = {
+                 expanded = !expanded
+             }
+         ) {
+             TextField(
+                 value = selectedText,
+                 onValueChange = { selectedText = it },
+                 label = { Text(text = "From:") },
+                 trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
+             )
+
+             val filteredOptions =
+                 cityDestinations.filter { it.contains(selectedText, ignoreCase = true) }
+             if (filteredOptions.isNotEmpty()) {
+                 ExposedDropdownMenu(
+                     expanded = expanded,
+                     onDismissRequest = {
+                         // We shouldn't hide the menu when the user enters/removes any character
+                     }
+                 ) {
+                     filteredOptions.forEach { item ->
+                         DropdownMenuItem(
+                             text = { Text(text = item) },
+                             onClick = {
+                                 selectedText = item
+                                 expanded = false
+                             }
+                         )
+                     }
+                 }
+             }
+         }
+     }
+ }
+
+ @OptIn(ExperimentalMaterial3Api::class)
+ @Composable
+ fun DropDownMenuCityDestination() {
+     val context = LocalContext.current
+     val cityDestinations = arrayOf("Stockholm", "Copenhagen", "Helsinki", "London", "Paris")
+     var expanded by remember { mutableStateOf(false) }
+     var selectedText by remember { mutableStateOf("") }
+
+     Box(
+         modifier = Modifier
+             .fillMaxWidth()
+             .padding(32.dp)
+     ) {
+         ExposedDropdownMenuBox(
+             expanded = expanded,
+             onExpandedChange = {
+                 expanded = !expanded
+             }
+         ) {
+             TextField(
+                 value = selectedText,
+                 onValueChange = { selectedText = it },
+                 label = { Text(text = "To:") },
+                 trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
+             )
+
+             val filteredOptions =
+                 cityDestinations.filter { it.contains(selectedText, ignoreCase = true) }
+             if (filteredOptions.isNotEmpty()) {
+                 ExposedDropdownMenu(
+                     expanded = expanded,
+                     onDismissRequest = {
+                         // We shouldn't hide the menu when the user enters/removes any character
+                     }
+                 ) {
+                     filteredOptions.forEach { item ->
+                         DropdownMenuItem(
+                             text = { Text(text = item) },
+                             onClick = {
+                                 selectedText = item
+                                 expanded = false
+                             }
+                         )
+                     }
+                 }
+             }
+         }
+     }
+ }
+
+ @OptIn(ExperimentalMaterial3Api::class)
+ @Composable
+ fun DropDownMenuImaginativeGoals() {
+     val context = LocalContext.current
+     val cityDestinations = arrayOf("The Shire To Mordor", "Around the world once", "Climb Mount Everest horizontally")
+     var expanded by remember { mutableStateOf(false) }
+     var selectedText by remember { mutableStateOf("") }
+
+     Box(
+         modifier = Modifier
+             .fillMaxWidth()
+             .padding(32.dp)
+     ) {
+         ExposedDropdownMenuBox(
+             expanded = expanded,
+             onExpandedChange = {
+                 expanded = !expanded
+             }
+         ) {
+             TextField(
+                 value = selectedText,
+                 onValueChange = { selectedText = it },
+                 label = { Text(text = "To:") },
+                 trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
+             )
+
+             val filteredOptions =
+                 cityDestinations.filter { it.contains(selectedText, ignoreCase = true) }
+             if (filteredOptions.isNotEmpty()) {
+                 ExposedDropdownMenu(
+                     expanded = expanded,
+                     onDismissRequest = {
+                         // We shouldn't hide the menu when the user enters/removes any character
+                     }
+                 ) {
+                     filteredOptions.forEach { item ->
+                         DropdownMenuItem(
+                             text = { Text(text = item) },
+                             onClick = {
+                                 selectedText = item
+                                 expanded = false
+                             }
+                         )
+                     }
+                 }
+             }
          }
      }
  }
